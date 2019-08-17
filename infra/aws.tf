6a50b1340cdf0d2341e30e588c726543a6cd9ae9
Effy Elden <git@effy.is>
2019-08-17 11:29:53 +1000

    Create an AWS CloudFront distribution

--- infra/aws.tf
+++ infra/aws.tf
@@ -44,3 +44,53 @@ resource "aws_acm_certificate" "certificate" {
     create_before_destroy = true
   }
 }
+
+resource "aws_cloudfront_distribution" "distribution" {
+  origin {
+    domain_name = aws_s3_bucket.bucket.website_endpoint
+    origin_id   = aws_s3_bucket.bucket.id
+
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  aliases = ["alltheclouds.app", "www.alltheclouds.app"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.bucket.id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+
+    default_ttl = 300
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.certificate.arn
+    ssl_support_method  = "sni-only"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
