bc164b4de212a74ec209bc8bea28e2ac861a7c67
Effy Elden <git@effy.is>
2019-08-17 11:02:20 +1000

    Create infra project with beginnings of AWS resources
    Includes AWS provider block, S3 bucket with public-read ACL & policy, and index.html S3 object in bucket with templating.

---
+++ infra/aws.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  version = "~> 2.24"
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "alltheclouds"
+  acl    = "public-read"
+  website {
+    index_document = "index.html"
+  }
+  policy = <<-POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AllowPublicRead",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::alltheclouds/*"
+    }
+  ]
+}
+POLICY
+
+}
+
+resource "aws_s3_bucket_object" "index_page" {
+  bucket  = aws_s3_bucket.bucket.id
+  key     = "index.html"
+  content = templatefile("../frontend/index.html", { cloud_provider = "Amazon Web Services" })
+}
+
